26abd07274443cc4f7c083a70afdb7b06f4e8525
TurnerD1 <TurnerD1@FCCAYALF255KYV>
2022-10-24 22:59:16 -0500

    Add project files.

---
+++ WebApplication1/Controllers/FormController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class FormController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Step1 step1)
+        {
+            var numbers = new List<int>();
+            return View("Form"); 
+        }
+    }
+}
---
+++ WebApplication1/Models/Step1.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Step1
+    {
+
+        [Range(0, 200)]
+        [Required]
+        public int MyNum { get; set; }
+    }
+}
